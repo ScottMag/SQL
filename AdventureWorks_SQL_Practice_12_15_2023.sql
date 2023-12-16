428efef9985f3019a1a0e2bb78eba1c568cd3d19
Scott Magouirk <47084935+ScottMag@users.noreply.github.com>
2023-12-15 23:01:16 -0600

    Add files via upload

---
+++ AdventureWorks_SQL_Practice_12_15_2023.sql
@@ -0,0 +1,28 @@
+-- ADVENTUREWORKS: SQL EXERCISES, PRACTICE, SOLUTION 
+-- HTTPS://WWW.W3RESOURCE.COM/SQL-EXERCISES/ADVENTUREWORKS/ADVENTUREWORKS-EXERCISES.PHP
+
+-- 1. FROM THE FOLLOWING TABLE WRITE A QUERY IN SQL TO RETRIEVE ALL ROWS AND COLUMNS 
+-- FROM THE EMPLOYEE TABLE IN THE ADVENTUREWORKS DATABASE. SORT THE RESULT SET IN 
+-- ASCENDING ORDER ON JOBTITLE. 
+
+SELECT *
+FROM HumanResources.Employee
+ORDER BY JobTitle ASC
+
+--------------------------------------------------------------------------------------
+-- 2. FROM THE FOLLOWING TABLE WRITE A QUERY IN SQL TO RETRIEVE ALL ROWS AND COLUMNS 
+-- FROM THE EMPLOYEE TABLE USING TABLE ALIASING IN THE ADVENTUREWORKS DATABASE. 
+-- SORT THE OUTPUT IN ASCENDING ORDER ON LASTNAME. 
+
+
+
+
+--------------------------------------------------------------------------------------
+
+
+
+
+
+--------------------------------------------------------------------------------------
+
+
